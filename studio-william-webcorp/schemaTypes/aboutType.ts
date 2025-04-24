c764f873f3af3b8bdb00e02e17ae90f326d53caf
Rizki Setyawan <rizki.setyawan@dansmultipro.com>
2025-04-24 22:03:04 +0700

    hide about percentage

--- studio-william-webcorp/schemaTypes/aboutType.ts
+++ studio-william-webcorp/schemaTypes/aboutType.ts
@@ -27,18 +27,18 @@ export const aboutType = defineType({
       type: 'boolean', // Changed to boolean
       description: 'Indicates if projects were completed with an additional bonus/plus.',
     }),
-    defineField({
-      name: 'qualityProvide',
-      title: 'Quality Provided (%)',
-      type: 'number',
-      validation: (Rule) => Rule.required().min(1).max(100), // Restrict to 1-100 range
-    }),
-    defineField({
-      name: 'projectPlanning',
-      title: 'Project Planning (%)',
-      type: 'number',
-      validation: (Rule) => Rule.required().min(1).max(100), // Restrict to 1-100 range
-    }),
+    // defineField({
+    //   name: 'qualityProvide',
+    //   title: 'Quality Provided (%)',
+    //   type: 'number',
+    //   validation: (Rule) => Rule.required().min(1).max(100), // Restrict to 1-100 range
+    // }),
+    // defineField({
+    //   name: 'projectPlanning',
+    //   title: 'Project Planning (%)',
+    //   type: 'number',
+    //   validation: (Rule) => Rule.required().min(1).max(100), // Restrict to 1-100 range
+    // }),
     defineField({
       name: 'photo',
       title: 'Photo',
