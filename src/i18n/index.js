eac56f8378b3b5bf2470bd595ac2ccbee1026730
Muhammad Zubair <muhammadzubair.dev01@gmail.com>
2025-05-25 21:19:47 +0700

    set default to id

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,19 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './en/translation.json';
 import id from './id/translation.json';
 
 i18n
-  .use(LanguageDetector) // Deteksi bahasa dari browser/user
+  // .use(LanguageDetector) // Deteksi bahasa dari browser/user
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
       id: { translation: id },
     },
-    fallbackLng: 'en', // fallback ke English jika tidak terdeteksi
+    lng: 'id', // ⬅️ Set default bahasa ke Bahasa Indonesia
+    fallbackLng: 'en', // fallback ke English jika tidak ada key yang tersedia dalam ID
     interpolation: {
       escapeValue: false, // tidak perlu escape pada React
     },
